ce82ad93e684816562c96d8334449bb3428b9f4b
Nicolas <nicolasrodriguezjr@hotmail.com>
2024-06-29 12:21:50 -0400

    Delete Countries

---
+++ Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,79 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Share.Entities;
+using System.Net;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+
+        public List<Country>? Countries { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Loadasync();
+        }
+
+        private async Task Loadasync()
+        {
+            var responseHttp = await Repository.GetAsync<List<Country>>("/api/countries");
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message, SweetAlertIcon.Error);
+                return;
+
+            }
+
+            Countries = responseHttp.Response;
+        }
+
+        private async Task DeleteAsync(Country country)
+        {
+            var result = await SweetAlertService.FireAsync(new SweetAlertOptions
+            {
+                Title = "Confirmacion",
+                Text = $"Estas seguro que deseas borrar el pais: {country.Name}",
+                Icon = SweetAlertIcon.Question,
+                ShowCancelButton = true,
+            });
+            var confirm = string.IsNullOrEmpty(result.Value);
+            if (confirm)
+            {
+                return;
+            }
+
+
+            var responseHttp = await Repository.DeleteAsync<Country>($"/api/countries/{country.Id}");
+            if (responseHttp.Error)
+            {
+                if (responseHttp.HttpResponseMessage.StatusCode == HttpStatusCode.NotFound)
+                {
+                    NavigationManager.NavigateTo("/countries");
+                }
+                else
+                {
+                    var messageError = await responseHttp.GetErrorMessageAsync();
+                    await SweetAlertService.FireAsync("Error", messageError, SweetAlertIcon.Error);
+
+                }
+                return;
+            }
+
+            await Loadasync();
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro borrado con exito.");
+        }
+    }
+}
---
+++ Orders.Frontend/Repositories/Repository.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.Json;
+using System.Text;
+
+namespace Orders.Frontend.Repositories
+{
+    public class Repository : IRepository
+    {
+        private readonly HttpClient _httpClient;
+
+        private JsonSerializerOptions _jsonDefaultOptions => new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+        public Repository(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<HttpResponseWrapper<object>> GetAsync(string url)
+        {
+            var responseHTTP = await _httpClient.GetAsync(url);
+            return new HttpResponseWrapper<object>(null, !responseHTTP.IsSuccessStatusCode, responseHTTP);
+        }
+
+        public async Task<HttpResponseWrapper<T>> GetAsync<T>(string url)
+        {
+            var responseHttp = await _httpClient.GetAsync(url);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswerAsync<T>(responseHttp, _jsonDefaultOptions);
+                return new HttpResponseWrapper<T>(response, false, responseHttp);
+            }
+            return new HttpResponseWrapper<T>(default, true, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContext = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PostAsync(url, messageContext);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<TResponse>> PostAsync<T, TResponse>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContext = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PostAsync(url, messageContext);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswerAsync<TResponse>(responseHttp, _jsonDefaultOptions);
+                return new HttpResponseWrapper<TResponse>(response, false, responseHttp);
+            }
+            return new HttpResponseWrapper<TResponse>(default, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<object>> DeleteAsync<T>(string url)
+        {
+            var responseHttp = await _httpClient.DeleteAsync(url);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<object>> PutAsync<T>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContext = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContext);
+            return new HttpResponseWrapper<object>(null, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        public async Task<HttpResponseWrapper<TResponse>> PutAsync<T, TResponse>(string url, T model)
+        {
+            var messageJSON = JsonSerializer.Serialize(model);
+            var messageContext = new StringContent(messageJSON, Encoding.UTF8, "application/json");
+            var responseHttp = await _httpClient.PutAsync(url, messageContext);
+            if (responseHttp.IsSuccessStatusCode)
+            {
+                var response = await UnserializeAnswerAsync<TResponse>(responseHttp, _jsonDefaultOptions);
+                return new HttpResponseWrapper<TResponse>(response, false, responseHttp);
+            }
+            return new HttpResponseWrapper<TResponse>(default, !responseHttp.IsSuccessStatusCode, responseHttp);
+        }
+
+        private async Task<T?> UnserializeAnswerAsync<T>(HttpResponseMessage httpRespnse, JsonSerializerOptions jsonSerializerOptions)
+        {
+            var respuestaString = await httpRespnse.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(respuestaString, jsonSerializerOptions);
+        }
+    }
+}
